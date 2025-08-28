dc4b92b54070ccc6266ee5bd83cd4d1474d7afe9
hemssy <hemsyeons@gmail.com>
2025-08-28 16:58:24 +0900

    1주차 2강 실습

---
+++ 1-2_Class/1-2_Class/Counter.swift
@@ -0,0 +1,18 @@
+//
+//  Counter.swift
+//  1-2_Class
+//
+//  Created by 신서연 on 8/28/25.
+//
+
+import Foundation
+
+class Counter {
+    var number: Int = 0
+    
+    func addCount() -> Int {
+        number += 1
+        return number
+    }
+}
+
